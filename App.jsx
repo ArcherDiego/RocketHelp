dc6afe9f4f92ea4e28d4f249791cef49b17caa1c
unknown <doliveirasantos98@gmail.com>
2022-07-20 18:22:10 -0300

    feat: Home page

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { SignIn } from './src/screen/SignIn';
+
+//import { SignIn } from './src/screen/SignIn';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screen/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -10,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
